ab87ee812b6aa4ccf22b60d39331b917a968268e
Vanessa  Carvalho <80365265+DevVane@users.noreply.github.com>
2021-06-30 12:19:29 -0300

    configurando conexao

--- src/conexao.js
+++ src/conexao.js
@@ -1,10 +1,13 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        user: 'postgres',
-        password: 'postgres',
-        database: 'market_cubos'
+        host: 'ec2-3-214-136-47.compute-1.amazonaws.com',
+        user: 'ofnxmnbbapgvew',
+        password: '7022d4bfaa5daf8f10e398166b8b8130a4c8d5b770d4efeda54a3ceefa377a80',
+        database: 'd8848fuabc46fp',
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 });
 
